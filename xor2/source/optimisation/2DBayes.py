9712669614e96e38718682c6e24412ecbdb8407e
Daniel Gordon <daniel.gordon@ed.ac.uk>
2018-01-23 23:42:45 +0000

    Combined Bayesian Optimization with CMC

---
+++ xor2/source/optimisation/2DBayes.py
@@ -0,0 +1,159 @@
+from __future__ import print_function
+from __future__ import division
+
+import numpy as np
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import matlab.engine
+import pickle
+
+
+from bayes_opt import BayesianOptimization
+
+
+# This function is a simple parabola with maximum at (5,5).
+def test_objective(thigh, shank):
+    return -((thigh-5)**2 + (shank-5)**2)
+
+
+def cmc_objective(thigh, shank):
+    print("Starting Matlab step.")
+    result = -eng.cmcObjective(np.asscalar(thigh), np.asscalar(shank)) # Be sure to test whether you can just pass this directly below, i.e. don't need to define cmc_objective.
+    print("This worked.")
+    return result
+
+
+def unique_rows(a):
+    """
+    A functions to trim repeated rows that may appear when optimizing.
+    This is necessary to avoid the sklearn GP object from breaking
+
+    :param a: array to trim repeated rows from
+
+    :return: mask of unique rows
+    """
+
+    # Sort array and kep track of where things should go back to
+    order = np.lexsort(a.T)
+    reorder = np.argsort(order)
+
+    a = a[order]
+    diff = np.diff(a, axis=0)
+    ui = np.ones(len(a), 'bool')
+    ui[1:] = (diff != 0).any(axis=1)
+
+    return ui[reorder]
+
+
+def posterior(bo, X):
+    ur = unique_rows(bo.X)
+    bo.gp.fit(bo.X[ur], bo.Y[ur])
+    mu, sigma2 = bo.gp.predict(X, return_std=True)
+    return mu, np.sqrt(sigma2), bo.util.utility(X, bo.gp, bo.Y.max())
+
+
+def plot_2d(name=None):
+
+    mu, s, ut = posterior(bo, X)
+
+    fig, ax = plt.subplots(2, 2, figsize=(14, 10))
+    gridsize=150
+
+    # fig.suptitle('Bayesian Optimization in Action', fontdict={'size':30})
+
+    # GP regression output
+    ax[0][0].set_title('Gausian Process Predicted Mean', fontdict={'size':15})
+    im00 = ax[0][0].hexbin(x, y, C=mu, gridsize=gridsize, cmap=cm.jet, bins=None, vmin=-0.9, vmax=2.1)
+    ax[0][0].axis([x.min(), x.max(), y.min(), y.max()])
+    ax[0][0].plot(bo.X[:, 1], bo.X[:, 0], 'D', markersize=4, color='k', label='Observations')
+
+    ax[0][1].set_title('Target Function', fontdict={'size':15})
+    im10 = ax[0][1].hexbin(x, y, C=z, gridsize=gridsize, cmap=cm.jet, bins=None, vmin=-0.9, vmax=2.1)
+    ax[0][1].axis([x.min(), x.max(), y.min(), y.max()])
+    ax[0][1].plot(bo.X[:, 1], bo.X[:, 0], 'D', markersize=4, color='k')
+
+
+    ax[1][0].set_title('Gausian Process Variance', fontdict={'size':15})
+    im01 = ax[1][0].hexbin(x, y, C=s, gridsize=gridsize, cmap=cm.jet, bins=None, vmin=0, vmax=1)
+    ax[1][0].axis([x.min(), x.max(), y.min(), y.max()])
+
+    ax[1][1].set_title('Acquisition Function', fontdict={'size':15})
+    im11 = ax[1][1].hexbin(x, y, C=ut, gridsize=gridsize, cmap=cm.jet, bins=None, vmin=0, vmax=8)
+
+    np.where(ut.reshape((300, 300)) == ut.max())[0]
+    np.where(ut.reshape((300, 300)) == ut.max())[1]
+
+    ax[1][1].plot([np.where(ut.reshape((300, 300)) == ut.max())[1]/50.,
+                   np.where(ut.reshape((300, 300)) == ut.max())[1]/50.],
+                  [0, 6],
+                  'k-', lw=2, color='k')
+
+    ax[1][1].plot([0, 6],
+                  [np.where(ut.reshape((300, 300)) == ut.max())[0]/50.,
+                   np.where(ut.reshape((300, 300)) == ut.max())[0]/50.],
+                  'k-', lw=2, color='k')
+
+    ax[1][1].axis([x.min(), x.max(), y.min(), y.max()])
+
+    for im, axis in zip([im00, im10, im01, im11], ax.flatten()):
+        cb = fig.colorbar(im, ax=axis)
+        # cb.set_label('Value')
+
+    if name is None:
+        name = '_'
+
+    plt.tight_layout()
+
+    # Save or show figure?
+    # fig.savefig('bo_eg_' + name + '.png')
+    plt.show()
+    plt.close(fig)
+
+
+if __name__ == "__main__":
+
+    # Start the MATLAB engine.
+    eng = matlab.engine.start_matlab()
+
+    # Set input parameters BO.
+    description = 'testing2D'
+    [min_thigh, max_thigh] = [0.0, 10.0]
+    [min_shank, max_shank] = [0.5, 11.5]
+    [x_thigh, x_shank] = [np.linspace(min_thigh, max_thigh, 10000).reshape(-1, 1),
+                          np.linspace(min_shank, max_shank, 10000).reshape(-1, 1)]
+
+    # Initialise and perform BO.
+    bo = BayesianOptimization(
+        cmc_objective, {'thigh': (min_thigh, max_thigh), 'shank': (min_shank, max_shank)})
+    trade_off = 5
+    max_iter = 50
+    bo.maximize(init_points=2, n_iter=0, acq='ucb', kappa=trade_off)
+
+    for n_iterations in range(2, max_iter):
+        print("hi")
+        bo.maximize(init_points=0, n_iter=1, kappa=trade_off)
+
+    # Save bo.
+    pickle.dump(bo, open("bo_save.p", "wb"))
+
+    # For plotting.
+    x = y = np.linspace(0, 6, 300)
+    X, Y = np.meshgrid(x, y)
+    x = X.ravel()
+    y = Y.ravel()
+    X = np.vstack([x, y]).T[:, [1, 0]]
+    z = test_objective(x, y)
+    fig, axis = plt.subplots(1, 1, figsize=(14, 10))
+    gridsize = 150
+
+    im = axis.hexbin(x, y, C=z, gridsize=gridsize, cmap=cm.jet, bins=None, vmin=-0.9, vmax=2.1)
+    axis.axis([x.min(), x.max(), y.min(), y.max()])
+
+    cb = fig.colorbar(im, )
+    cb.set_label('Value')
+
+    plot_2d("{:03}".format(len(bo.X)))
+    plt.ioff()
+    plot_2d("{:03}".format(len(bo.X)))
+    plt.savefig(description + '.png', orientation='landscape', bbox_inches='tight')
---
+++ xor2/source/optimisation/cmcObjective.m
@@ -0,0 +1,208 @@
+function hamstringPower = cmcObjective(thigh, shank)
+
+
+%% Create the modified model file. 
+% Import OpenSim modelling classes.
+import org.opensim.modeling.*
+
+% Some defaults. 
+model_path = ...
+    'ReferenceData\XoR2-correct-bushing-locations-new-default-coords.osim';
+save_dir = ['thigh=' num2str(thigh) 'shank=' num2str(shank)];
+save_name = 'model.osim';
+
+% Convert input to bushing locations.
+thigh_location_y = -0.17 - thigh/100;
+
+shank_location_x = 0.09 - (shank/100 + 0.10)*tan(deg2rad(14.5));
+shank_location_y = -0.13 - shank/100;
+
+% Read the default model file using xmlread.
+xml_model = xmlread(model_path);
+
+% Read in the actual model file using the OpenSim API, get access to
+% initialise the model state, and get access to the bodies. 
+model = Model(model_path);
+state = model.initSystem();
+markers = model.getMarkerSet();
+bodies = model.getBodySet();
+test_marker = markers.get('test_marker');
+
+% Find all the weld joints.
+weld_joints = xml_model.getElementsByTagName('WeldJoint');
+
+% Body names we need. 
+body_names.human = {'femur_r', 'tibia_r', 'femur_l', 'tibia_l'};
+body_names.xor2 = {'XoR2_thigh_r', 'XoR2_shank_r', 'XoR2_thigh_l', ...
+    'XoR2_shank_l'};
+
+% Correctly ordered strings to look for for the joints. 
+joint_names.human = {'bushing_thigh_r_weld', 'bushing_shank_r_weld', ...
+    'bushing_thigh_l_weld', 'bushing_shank_l_weld'};
+joint_names.xor2 = {'bushing_XoR2_thigh_r_weld', ...
+    'bushing_XoR2_shank_r_weld', 'bushing_XoR2_thigh_l_weld', ...
+    'bushing_XoR2_shank_l_weld'};
+
+% Loop through the weld joints setting the new locations, using file 
+% structure knowledge, currently XoR2 links only.
+saved_b_pos{4} = {};
+looking_for = 1;
+for i=0:weld_joints.getLength()-1
+    if looking_for > 4
+        break
+    elseif strcmp(weld_joints.item(i).getAttribute('name'), ...
+            joint_names.xor2{looking_for})
+        % Get the current bushing position. 
+        b_pos = str2double(strsplit(string(weld_joints.item(i). ...
+            getElementsByTagName('location_in_parent').item(0). ...
+            getTextContent())));
+        
+        % Replace b_pos with the new bushing location. 
+        if mod(looking_for,2) == 0
+            b_pos(1) = shank_location_x;
+            b_pos(2) = shank_location_y;
+        else
+            b_pos(2) = thigh_location_y;
+        end
+        
+        % Save the b_pos array. 
+        saved_b_pos{looking_for} = b_pos;
+        
+        % Convert b_pos to a string and set it.
+        b_pos = [num2str(b_pos(1)) ' ' num2str(b_pos(2)) ' ' ...
+            num2str(b_pos(3))];
+        weld_joints.item(i).getElementsByTagName('location_in_parent'). ...
+            item(0).setTextContent(b_pos);
+        
+        % Increment what we're looking for.
+        looking_for = looking_for + 1;
+    end
+end
+
+% Do the same again but for the human links. 
+looking_for = 1;
+for i=0:weld_joints.getLength()-1
+    if looking_for > 4
+        break
+    elseif strcmp(weld_joints.item(i).getAttribute('name'), ...
+            joint_names.human{looking_for})
+        % Get the current bushing position. 
+        b_pos = saved_b_pos{looking_for};
+        
+        % Convert b_pos to the frame of reference of the appropriate 
+        % human link.
+        test_marker.changeBody(bodies.get(body_names.xor2{looking_for}));
+        test_marker.setOffset(b_pos);
+        test_marker.changeBodyPreserveLocation(...
+            state, bodies.get(body_names.human{looking_for}));
+        b_pos_converted = test_marker.getOffset();
+        
+        % Convert b_pos_converted to a string and set it.
+        b_pos_converted = [num2str(b_pos_converted.get(0)) ' ' ...
+            num2str(b_pos_converted.get(1)) ' ' ...
+            num2str(b_pos_converted.get(2))];
+        weld_joints.item(i).getElementsByTagName(...
+            'location_in_parent').item(0).setTextContent(b_pos_converted);
+        
+        % Increment what we're looking for.
+        looking_for = looking_for + 1;
+    end
+end
+
+% Names of the bodies we need - had to reorder.. 
+body_names.human{1} = 'femur_l';
+body_names.xor2{1} = 'XoR2_thigh_l';
+body_names.human{2} = 'tibia_l';
+body_names.xor2{2} = 'XoR2_shank_l';
+body_names.human{3} = 'femur_r';
+body_names.xor2{3} = 'XoR2_thigh_r';
+body_names.human{4} = 'tibia_r';
+body_names.xor2{4} = 'XoR2_shank_r';
+
+% Find all the Bushing forces.
+bushing_forces = xml_model.getElementsByTagName('BushingForce');
+
+% Correctly ordered strings to look for for the bushings. 
+names = {'LeftThighBushing', 'LeftShankBushing', 'RightThighBushing', ...
+    'RightShankBushing'};
+
+% Loop through the weld joints setting the new locations, using file 
+% structure knowledge. 
+looking_for = 1;
+for i=0:bushing_forces.getLength()-1
+    if looking_for > 4
+        break
+    elseif strcmp(bushing_forces.item(i).getAttribute('name'), ...
+            names{looking_for})
+        % Get the current bushing position in the frame of reference of 
+        % XoR2. 
+        b_pos = str2double(strsplit(string(bushing_forces.item(i). ...
+            getElementsByTagName('location_body_1').item(0). ...
+            getTextContent())));
+        
+        % Replace b_pos with the new bushing location. 
+        if looking_for == 2 || looking_for == 4
+            b_pos(1) = shank_location_x;
+            b_pos(2) = shank_location_y;
+        elseif looking_for < 5
+            b_pos(2) = thigh_location_y;
+        end
+        
+        % Convert b_pos to the frame of reference of the appropriate 
+        % human link.
+        test_marker.changeBody(bodies.get(body_names.xor2{looking_for}));
+        test_marker.setOffset(b_pos);
+        test_marker.changeBodyPreserveLocation(...
+            state, bodies.get(body_names.human{looking_for}));
+        b_pos_converted = test_marker.getOffset();
+        
+        % Convert b_pos to a string and set it.
+        b_pos = [num2str(b_pos(1)) ' ' num2str(b_pos(2)) ' ' ...
+            num2str(b_pos(3))];
+        bushing_forces.item(i).getElementsByTagName(...
+            'location_body_1').item(0).setTextContent(b_pos);
+        
+        % Convert b_pos_converted to a string and set it.
+        b_pos_converted = [num2str(b_pos_converted.get(0)) ' ' ...
+            num2str(b_pos_converted.get(1)) ' ' ...
+            num2str(b_pos_converted.get(2))];
+        bushing_forces.item(i).getElementsByTagName(...
+            'location_body_2').item(0).setTextContent(b_pos_converted);
+        
+        % Increment what we're looking for.
+        looking_for = looking_for + 1;
+    end
+end
+
+% Write the updated model file.
+xmlwrite([save_dir filesep save_name], xml_model);
+
+%% Perform CMC for 0.5s. 
+results_folder = ['C:\Users\Daniel\Documents\GitHub\atr\xor2\source\optimisation\' save_dir filesep 'CMC'];
+cmc = CMCTool('ReferenceData\testCMC_TuningBushings.xml');
+cmc.setModelFilename(['C:\Users\Daniel\Documents\GitHub\atr\xor2\source\optimisation\' save_dir filesep save_name]);
+cmc.loadModel('ReferenceData\testCMC_TuningBushings.xml');
+cmc.updateModelForces(cmc.getModel(), 'ReferenceData\testCMC_TuningBushings.xml');
+mkdir(results_folder);
+cmc.setResultsDir(results_folder);
+success = cmc.run();
+
+if success
+    result = Data(['C:\Users\Daniel\Documents\GitHub\atr\xor2\source\optimisation\' save_dir filesep 'CMC' filesep 'XoR2-Human_Actuation_power.sto']);
+    powers = result.getDataCorrespondingToLabel('semimem_l') + ...
+        result.getDataCorrespondingToLabel('semiten_l') + ...
+        result.getDataCorrespondingToLabel('bifemlh_l') + ...
+        result.getDataCorrespondingToLabel('bifemsh_l') + ...
+        result.getDataCorrespondingToLabel('semimem_r') + ...
+        result.getDataCorrespondingToLabel('semiten_r') + ...
+        result.getDataCorrespondingToLabel('bifemlh_r') + ...
+        result.getDataCorrespondingToLabel('bifemsh_r');
+    hamstringPower = sum(powers)/vectorSize(powers); % Average power consumption. 
+else
+    hamstringPower = 1000;
+end
+
+% If positive, hamstrings applied energy to model, if negative, hamstrings
+% overall took in energy. 
+
+end
